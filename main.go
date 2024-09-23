d25e32451bdfc872685d562b169f9a7d93d52358
YolanC13 <yolan.chiotti@ynov.com>
2024-09-23 11:29:55 +0200

    Amélioration de main.go

--- main.go
+++ main.go
@@ -3,39 +3,66 @@ package main
 import (
 	"fmt"
 	hangman "hangman/Internals"
+	"math/rand"
+
+	"strings"
 
 	"github.com/fatih/color"
-	//"github.com/fatih/color"
 )
 
 func main() {
 	hangman.Letters = &[]string{}
-	hangman.PressF11()
+	//hangman.PressF11()
 	hangman.ClearScreen()
 
 	hangman.DisplayText(hangman.DisplayTextOptions{
 		TextToPrint: "Choisit un mot à faire deviner:",
 	})
+
 	input := hangman.GetInput()
-	InitializeHangman(input)
+	if input == "" || !IsLetter(input) {
+		main()
+	}
+	InitializeVariables(input)
 }
 
 func InitializeHangman(text string) {
 	hangman.ClearScreen()
+	for i := 0; i < len(*hangman.Letters); i++ {
+		fmt.Print(string((*hangman.Letters)[i]))
+	}
+	hangman.NewLine(1)
+	for j := 0; j < len(*hangman.Letters); j++ {
+		if (*hangman.Letters)[j] == "_" {
+			break
+		} else {
+			if j == len(*hangman.Letters)-1 {
+				hangman.DisplayText(hangman.DisplayTextOptions{
+					TextToPrint: "Tu as gagné !",
+					FgColor:     color.FgGreen,
+				})
+				return
+			}
+		}
+	}
+	getLetter()
+}
+
+func InitializeVariables(text string) {
 	for i := 0; i < len(text); i++ {
 		if text[i] == ' ' {
 			hangman.HangmanChar = append(hangman.HangmanChar, " ")
 			*hangman.Letters = append(*hangman.Letters, " ")
-			fmt.Print(" ")
 		} else {
-			hangman.HangmanChar = append(hangman.HangmanChar, string(text[i]))
+			hangman.HangmanChar = append(hangman.HangmanChar, strings.ToLower(string(text[i])))
 			*hangman.Letters = append(*hangman.Letters, "_")
-			fmt.Print("_")
 		}
 	}
-	hangman.Characters = &hangman.HangmanChar
-	hangman.NewLine(1)
-	getLetter()
+	if len(*hangman.Letters) > 5 {
+		x := rand.Intn(len(*hangman.Characters))
+		(*hangman.Letters)[x] = (*hangman.Characters)[x]
+	}
+	InitializeHangman(text)
 }
 
 func getLetter() {
@@ -46,12 +73,18 @@ func getLetter() {
 		TextToPrint: "Choisit une lettre",
 	})
 	input := hangman.GetInput()
-	if len(input) != 1 {
+	if input == "" || !IsLetter(input) {
+		hangman.DisplayText(hangman.DisplayTextOptions{
+			TextToPrint: "Veuillez entrer seulement des lettres",
+			FgColor:     color.FgRed,
+		})
+		getLetter()
+	} else if len(input) != 1 {
 		hangman.DisplayText(hangman.DisplayTextOptions{
 			TextToPrint: "Veuillez entrer une seule lettre",
 			FgColor:     color.FgRed,
 		})
-		return
+		getLetter()
 	} else {
 		checkLetter(input)
 	}
@@ -92,4 +125,13 @@ func PrintHangman() {
 		}
 	}
 	getLetter()
+}
+
+func IsLetter(s string) bool {
+	for _, r := range s {
+		if (r < 'a' || r > 'z') && (r < 'A' || r > 'Z') && r != ' ' {
+			return false
+		}
+	}
+	return true
 }
